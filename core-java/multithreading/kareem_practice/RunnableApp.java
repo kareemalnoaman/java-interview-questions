ed4a166b87d06d166b4fc625a8305824ed7a4d07
Kareem Alnoaman <abdul.alnoaman@gmail.com>
2023-12-26 21:37:43 -0600

    Practice threading

---
+++ core-java/multithreading/kareem_practice/RunnableApp.java
@@ -0,0 +1,9 @@
+public class RunnableApp{
+
+  public static void main(String args[]){
+
+    RunnableDemo rd = new RunnableDemo();
+    Thread t1 = new Thread(rd);
+    t1.start();
+  }
+}
---
+++ core-java/multithreading/kareem_practice/RunnableDemo.java
@@ -0,0 +1,6 @@
+public class RunnableDemo implements Runnable{
+  @override
+  public void run(){
+    System.out.print("My runnable demo threads");
+  }
+}
